32909c1a858cc9c4d803c1ce3ce7a044b755b67d
mallicksm <mallicksm@gmail.com>
2026-03-31 00:12:11 -0700

    converted cli to task

--- riscv/main.c
+++ riscv/main.c
@@ -1,7 +1,16 @@
 #include <stdint.h>
 #include "cli.h"
+#include "proc.h"
+
+static struct task cli_task;
+static uint8_t cli_stack[1024];
 
 int main(void) {
-   cli();
+   sched_init();
+   task_init(&cli_task, "cli", cli, cli_stack, sizeof(cli_stack));
+   sched_start();
+
+   for (;;)
+      ;
    return 0;
 }
