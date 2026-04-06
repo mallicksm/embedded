82b0caaf097a7f06ebc1b92f808c9259de6b9077
mallicksm <mallicksm@gmail.com>
2026-04-06 00:15:42 -0700

    allregs

--- riscv/gdb/gdb_csr.py
+++ riscv/gdb/gdb_csr.py
@@ -2,26 +2,37 @@ import gdb
 
 def csr(name):
    try:
-      return int(gdb.parse_and_eval(f"${name}"))
+      return int(gdb.parse_and_eval(f"${name}")) & 0xffffffff
    except:
       return None
 
 def pr(name, val):
    if val is None:
-      print(f"{name:8} : <na>")
+      print(f"{name:10} : <na>")
    else:
-      print(f"{name:8} : 0x{val:08x}")
+      print(f"{name:10} : 0x{val:08x}")
 
 class DumpCSR(gdb.Command):
    def __init__(self):
       super().__init__("dumpcsr", gdb.COMMAND_USER)
 
    def invoke(self, arg, from_tty):
-      print("\n=== CSR ===")
-
-      for n in ["mstatus", "mepc", "mcause", "mtvec"]:
+      print("\n=== CSR: trap ===")
+      for n in ["mepc", "mcause", "mtval", "mtvec"]:
          pr(n, csr(n))
 
-      print("============\n")
+      print("\n=== CSR: state ===")
+      for n in ["mstatus", "misa"]:
+         pr(n, csr(n))
+
+      print("\n=== CSR: interrupt ===")
+      for n in ["mie", "mip"]:
+         pr(n, csr(n))
+
+      print("\n=== CSR: misc ===")
+      for n in ["mscratch"]:
+         pr(n, csr(n))
+
+      print("\n=================\n")
 
 DumpCSR()
